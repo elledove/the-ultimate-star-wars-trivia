ef4d8c47fc474b3459ded4691ea11eb9a24b489a
Lauren Dove <elledoveinc@gmail.com>
2020-05-09 22:56:50 -0400

    create appContainer class w/functions

---
+++ the-ultimate-star-wars-trivia-client/src/appContainer.js
@@ -0,0 +1,22 @@
+class AppContainer{
+
+    questions = []
+    themes = []
+    url = ""
+   score = {}
+
+
+   getQuestions(){
+   // make fetch request to /questions, then call renderQuestions
+
+     }
+
+   renderQuestions(){
+   // creat DOM nodes and insert data into them to render in the DOM 
+
+  
+   }
+
+
+   
+}
